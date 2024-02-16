e86858c8447cfc6fbe452f0a36cd68aaa6ec2743
Alexshch09 <alexshch09@gmail.com>
2024-02-16 17:02:05 +0100

    first commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from blueprints.views import views_blueprint
 
 app = Flask(__name__)
 
-app.register_blueprint(views_blueprint, url_prefix='/pages')
+app.register_blueprint(views_blueprint)
 
 if __name__ == "__main__":
     app.run()
